441ba4a24ce71285907443afec37928884364e86
balajidasika-oss <balajidasika@gmail.com>
2025-12-20 14:50:44 +0530

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,10 +1,10 @@
 class javads{
 public static void main (String[] args){
 int arr[] = {1, 2, 3, 4};
-System.out.println("arr is "arr[1]);//
+System.out.println("arr is " +arr[1]);
 float fr[] = {1, 2, 3, 4};
 
-System.out.println(" float is : "fr[0]);
+System.out.println(" float is : " +fr[0]);
 
 }
 }
